a6001e3a447c5847303868f3cff7cacd5fe2984d
Noor Al Darraji <077068@yahoo.com>
2019-08-23 11:58:43 -0500

    Finished Login page :hammer:

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,74 @@
 import React from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+function Login({ touched, errors }) {
+
+  const token = localStorage.getItem("token");
+  if (token) {
+    return <Redirect to="/bubblepage" />;
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <Form className="form">
+      <div className="form-group">
+        <label className="label">User name</label>
+        <Field
+          className="input"
+          name="username"
+          type="username"
+          autoComplete="off"
+        />
+        <p>{touched.username && errors.username}</p>
+      </div>
+      <div className="form-group">
+        <label className="label">Password</label>
+        <Field
+          className="input"
+          name="password"
+          type="password"
+          autoComplete="off"
+        />
+      </div>
+      <p>{touched.password && errors.password}</p>
+      <button className="btn">Submit &rarr;</button>
+    </Form>
   );
-};
+}
 
-export default Login;
+
+// make a post request to retrieve a token from the api
+// when you have handled the token, navigate to the BubblePage route
+
+
+export default withFormik({
+  mapPropsToValues() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    username: Yup.string()
+      .required(),
+    password: Yup.string()
+      .required()
+      .min(6)
+  }),
+  handleSubmit(values, formikBag) {
+
+    const url = "http://localhost:5000/api/login";
+    axios
+      .post(url, values)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        console.log('%c Token has been saved, re-routing to bubblepage...', 'color: green;') // CSS in console log xD
+        formikBag.props.history.push("/bubblepage");
+      })
+      .catch(errors => {
+        console.log(errors.response.data);
+      });
+  }
+})(Login);
